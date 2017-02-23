4f983d09012c9f27d23d55c0a1346cad8e92435f
didietsuryadi <didietsuryadi>
2017-02-23 13:30:06 +0700

    setting-app

--- server/app.js
+++ server/app.js
@@ -4,18 +4,28 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose')
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var cors = require('cors')
+var passport = require('passport')
+require('dotenv').config()
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/adminpanel', function (err) {
+  if (err) throw err
+  console.log('database connected using mongoose')
+})
 
+// ====== Passport ======
+
+app.use(passport.initialize())
+require('./config/passport')(passport)
+
+// ======= END =======
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,7 +50,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err);
 });
 
 module.exports = app;
